69daa8847d0af172cb990dd4862a3555507e7413
fei.he <fei.he@xquant.com>
2020-06-20 17:24:23 +0800

    init

--- normal_portal/src/main/java/com/normal/portal/App.java
+++ normal_portal/src/main/java/com/normal/portal/App.java
@@ -2,6 +2,7 @@ package com.normal.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 
 @SpringBootApplication
 public class App {
