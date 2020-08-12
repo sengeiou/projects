b98172dce3930073fab79bfc6d577a26573b57e1
fei.he <hefeixxx@gmail.com>
2020-08-12 23:13:56 +0800

    '已完成提交'

--- normal_biz_assistant/src/main/java/com/normal/bizassistant/autosend/TaobaoSendGoodResponse.java
+++ normal_biz_assistant/src/main/java/com/normal/bizassistant/autosend/TaobaoSendGoodResponse.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class TaobaoSendGoodResponse extends TaobaoResponse implements SendGoodResponse {
 
 
+
     @Override
     public String text() {
         return null;
